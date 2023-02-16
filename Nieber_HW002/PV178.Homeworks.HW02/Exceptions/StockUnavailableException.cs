854906f8d45720b588e288d40526d899d693b67a
JakubDuch7790 <101965286+JakubDuch7790@users.noreply.github.com>
2023-02-16 12:24:01 +0100

    Adding second small project

---
+++ Nieber_HW002/PV178.Homeworks.HW02/Exceptions/StockUnavailableException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PV178.Homeworks.HW02.Exceptions
+{
+    /// <summary>
+    /// Thrown when available amount of stock is not sufficient to satisfy user order
+    /// </summary>
+    public class StockUnavailableException
+    {
+        
+    }
+}

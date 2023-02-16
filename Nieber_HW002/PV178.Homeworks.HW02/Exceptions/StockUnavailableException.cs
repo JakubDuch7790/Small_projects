7515e7856bcd529cb829e1658b96c4bd5e2f118d
MladejLeopard <jakub.duch@gmail.com>
2023-02-16 15:02:52 +0100

    Added custom exeption in StockUnavailableExeption class Added Coordinates class with implementation

--- Nieber_HW002/PV178.Homeworks.HW02/Exceptions/StockUnavailableException.cs
+++ Nieber_HW002/PV178.Homeworks.HW02/Exceptions/StockUnavailableException.cs
@@ -5,8 +5,18 @@ namespace PV178.Homeworks.HW02.Exceptions
     /// <summary>
     /// Thrown when available amount of stock is not sufficient to satisfy user order
     /// </summary>
-    public class StockUnavailableException
+    public class StockUnavailableException : Exception
     {
-        
+        public StockUnavailableException()
+        {
+        }
+
+        public StockUnavailableException(string message) : base(message)
+        {
+        }
+
+        public StockUnavailableException(string message, Exception innerException) : base(message, innerException)
+        { 
+        }
     }
 }
---
+++ Nieber_HW002/PV178.Homeworks.HW02/Model/Coordinates.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PV178.Homeworks.HW02.Model
+{
+    internal class Coordinates
+    {
+        public int RowIndex { get; private set; }
+
+        public char ColumnIndex { get; private set; }
+
+        public static Coordinates Empty { get; private set; }
+            
+        public Coordinates(int rowIndex, char columnIndex)
+        {
+            RowIndex = rowIndex;
+            ColumnIndex = columnIndex;
+        }
+
+        public override string ToString()
+        {
+            return $"{RowIndex};{ColumnIndex}";
+        }
+    }
+}
